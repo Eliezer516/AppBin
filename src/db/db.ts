5e83dddae7279947746538f644c6b6c9fa8116a0
Eliezer516 <ybarretomaterano@gmail.com>
2025-10-27 11:03:28 -0400

    se eliminaron archivos innecesarios

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,4 +1,15 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
+import { apps } from './schema'
 
 const db = drizzle(process.env.DB_FILE_NAME!);
+
+
+const getApps = async () => {
+  const result = await db.select().from(apps)
+  return result
+}
+
+export {
+  getApps
+}
