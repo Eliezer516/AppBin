4e0cb3ef33c767cea1bf9d51c8b65184469bfd57
Eliezer Dev. <ybarretomaterano@gmail.com>
2025-10-29 14:16:35 +0000

    se añadieron nuevas acciones para la gestión de "Bins", incluyendo la creación, obtención y listado de "Bins"; se actualizó el modal para mostrar el ID del nuevo "Bin" creado y se ajustó la página de detalles del "Bin" para utilizar la nueva acción

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,6 @@
 import {defineAction} from 'astro:actions'
 import { z } from 'astro:schema'
-import { db, Bins } from 'astro:db'
+import { db, Bins, eq } from 'astro:db'
 import { uid } from 'uid'
 
 export const server = {
@@ -10,11 +10,28 @@ export const server = {
       code: z.string()
     }),
     handler: async (input) => {
-      await db.insert(Bins).values({
+      const result = await db.insert(Bins).values({
         id: uid(),
         title: input.title,
         code: input.code
-      })
+      }).returning()
+      console.log(result)
+      return result
+    }
+  }),
+  getAllBins: defineAction({
+    handler: async () => {
+      const result = db.select().from(Bins)
+      return result
+    }
+  }),
+  getBin: defineAction({
+    input: z.object({
+      id: z.string()
+    }),
+    handler: async (input) => {
+      const result = db.select().from(Bins).where(eq(Bins.id, input.id))
+      return result
     }
   })
 }
