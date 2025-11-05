8295d87d6da729c9d385c36d7c566017c4b08f0c
Eliezer516 <ybarretomaterano@gmail.com>
2025-11-05 11:01:42 -0400

    add: autenticacion con auth-astro

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+// import { defineMiddleware } from "astro:middleware";
+// import { getSession } from 'auth-astro/server';
+
+// const protectedRoutes = [
+//   "/",
+//   "/editor"
+// ];
+
+// export const onRequest = defineMiddleware(async ( request, next) => {
+//   const { locals, url, cookies, redirect } = request
+//   if (protectedRoutes.some(protectedRoute => protectedRoute === url.pathname)) {
+//     const session = await getSession(request)
+//     console.log(session)
+//     if(!session) {
+//       return redirect("/login")
+//     }
+//   }
+//   next()
+//   },
+// );
